271a7cf0f0fd7a0ad352adfc7a7035966056079d
Justine Dakiss <justinedaquis28@gmail.com>
2023-11-02 17:12:39 +0800

    established game logic

--- main.js
+++ main.js
@@ -10,125 +10,159 @@
 // 0,4,8
 // 2,4,6
 
-// HTML SkELETON SETUP
+const playerOne = createPlayer('julius', 'X')
+const playerTwo = createPlayer('mark', 'O')
 const board = document.querySelector('.board');
+
+
+let position;
+let result;
+let current = true;
+let next = false;
 
 for (let i=0; i < 9; i++) {
     console.log('added a cell!')
     const cell = document.createElement('div');
     cell.classList.add('cell')
-    cell.addEventListener('click', () => {
-        console.log('clicked a cell!')
+    cell.addEventListener('click', (element) => {
+        // cell is clicked!
+        position = cellArray.indexOf(cell);
+        
+        turn = current == true ? playerOne : playerTwo
+        
+        result = gameBoard.gameBoardUpdate(turn.symbol,position);
+
+
+        if (gameBoard.winCheck().getResult()) {
+            // if there is a winner!
+            
+            gameBoard.winCheck().getWinningPlayer().addScore();
+            console.log(gameBoard.winCheck().getWinningPlayer().name)
+            console.log(gameBoard.winCheck().getWinningPlayer().getScore() + ' <-- score');
+            gameBoard.boardReset();
+            current = true;
+            next = false;
+        } 
+        
+        if (gameBoard.drawCheck()) {
+            // if there is a draw!
+            // start over!
+            gameBoard.boardReset();
+            current = true;
+            next = false;
+        }
+        
+        if (result) {
+            // move is complete! or valid
+            current = !current;
+            next = !next;
+        } 
+
+        boardRefresh(gameBoard.gameArray, cellArray)
+        
+
+        // if move is valid, then we move to player two
+        
     })
     board.appendChild(cell)
+
 }
 
+const cellArray = Array.from(board.children);
 
+function boardRefresh(gameArray,cellArray) {
+    for (let i=0;i<9;i++) {
+
+        gameArray[i] == 'empty' ? cellArray[i].textContent = '' : cellArray[i].textContent = gameArray[i];
+    }
+
+}
 
 
 
 const gameBoard = {
     gameArray: Array(9).fill('empty'),
-
     gameBoardUpdate: function (symbol,pos) {
         if (this.gameArray[pos] == 'empty') {
+            // console.log(this.gameArray[pos] + '<-- gameboard update function ')
             this.gameArray[pos] = symbol
             return true
         } else {
-            console.log('position taken!') 
             return false
         }
         },
     
     winCheck: function() {
-        console.log('win check function start....')
-
-        // loops through given array!
-        // three directions
-        let start;
-        let end;
         let arraySet;
         let stagingArr;
-        // ROWS 
-        // 
-        start = 0;
-        end = 3;
+        let index;
+        let result = false;
+        // default is no one won (false)
+        let getResult = () => result;
+        let getWinningPlayer = () => playerOne.symbol == arraySet[0] ? playerOne : playerTwo;
+
+        let start = 0;
+        let end = 3;
         for (i = 0; i < 3; i++) {
-            // array
             arraySet = [... new Set(this.gameArray.slice(start, end))]
-            // loops through next row
+            // console.log(this.gameArray.slice(start,end) + '<-- sliced')
+
             start = end;
             end = end + end;
-            // if no unique elements (all elements in row is the same)
-            // return true
-            if (arraySet.length == 1) {
+            if (arraySet.length == 1 && arraySet[0] != 'empty') {
                 console.log('row winner')
-                return true;
-            }
-            
-        }
+                result = true; 
 
-        // COLUMNS 
-        // 
+                return {getResult, getWinningPlayer};
+            }
+        }
         start = 0;
-        let index;
-        // change starting point of loop
-        // 0,1,2 = i
-        // for every iteration, we will add + 3 positions
         for (let i = 0; i < 3; i++) {
-            // for 3 loops
             stagingArr = []
             index = start;
             for (let i = 0; i < 3; i++) {
-                // set starting column as starting index to loop
-                // push to staging array
                 stagingArr.push(this.gameArray[index])
-                // add +3 pos to check second row
-                // console.log(index)
                 index += 3;
             }
-            // move on to next column
             start++
-            // check if no unique
-            // console.log('staging array: ')
-            // console.log(stagingArr)
             arraySet = [... new Set(stagingArr)];
-            // console.log('array set: ')
-            // console.log(arraySet)
-            // console.log(arraySet.length + "<== array's length!")
-
-            if (arraySet.length == 1) {
+            if (arraySet.length == 1 && arraySet[0] != 'empty') {
                 console.log('column winner')
-                return true;
+                result = true; 
+
+                return {getResult, getWinningPlayer};
             }
         }
-
-        // DIAGONAL
-        // -1,4,8
-        // 1,4,6
-        start = 1;
-        let skip = 2
+        // 0,4,8
+        // 2,4,6
+        start = 0;
+        let skip = 4
         for (let i = 0; i < 2; i++) {
-
             stagingArr = []
-            for (i = 0; i < 3; i++) {
+            for (let i = 0; i < 3; i++) {
                 index = start;
                 stagingArr.push(this.gameArray[start])
-                start += skip;
+                console.log(start + '<-- start index')
+                start += skip;  
 
             }
-            start -= 2;
-            skip += 2;
+            console.log(stagingArr)
+            start = 2;
+            skip -= 2;
             arraySet = [... new Set(stagingArr)];
-            if (arraySet.length == 1) {
+            if (arraySet.length == 1 && arraySet[0] != 'empty') {
                 console.log('diagonal winner')
-                return true;
+                
+                result = true; 
+
+                return {getResult, getWinningPlayer};
+                
             }
         }
         
-
-        return false;
+        return {getResult};
     },
+    
     drawCheck: function() {
         let count = 0;
         this.gameArray.forEach(element => {
@@ -137,15 +171,15 @@ const gameBoard = {
             }
         })
         return count == 0;
+    },
+
+    boardReset: function() {
+        this.gameArray = Array(9).fill('empty')
     }
 }
 
 
-
-
-
-
-function createPlayer(name) {
+function createPlayer(name, symbol) {
     
     let score = 0; 
     const addScore = () => score++;
@@ -154,53 +188,25 @@ function createPlayer(name) {
 }
 
 
+// let turn;
 
-gameBoard.gameBoardUpdate('O',0)
-gameBoard.gameBoardUpdate('X',1)
-gameBoard.gameBoardUpdate('O',2)
+// while (winYet()) {
 
-gameBoard.gameBoardUpdate('O',3)
-gameBoard.gameBoardUpdate('O',4)
-gameBoard.gameBoardUpdate('X',5)
-
-gameBoard.gameBoardUpdate('X',6)
-gameBoard.gameBoardUpdate('O',7)
-gameBoard.gameBoardUpdate('O',8)
-
-console.log(gameBoard.drawCheck())
-
-
-const res = gameBoard.winCheck('X') 
-
-const playerOne = createPlayer('julius', 'X')
-const playerTwo = createPlayer('mark', 'O')
-
-function winYet() { 
-    // function checks whether there is a winner  
-    if (playerOne.getScore() < 3 && playerTwo.getScore() < 3) {    
-        return true
-    } else {
-        return false;
-    }   
-}
-
-function getWinner() {
-    let winner;
-    playerOne.getScore() > playerTwo.getScore() ? winner = playerOne : winner = playerTwo;
-}
-
-while (winYet()) {
-    // first move!
+//     // first move!
+//     // player one!
+//     turn = playerOne
     
+//     gameBoard.gameBoardUpdate(position,turn.symbol)
+//     console.log(gameBoard.gameArray)
 
 
 
 
 
-    playerOne.addScore();
-    console.log(playerOne.getScore())
+//     playerOne.addScore();
+//     console.log(playerOne.getScore())
 
 
 
 
-}
+// }
